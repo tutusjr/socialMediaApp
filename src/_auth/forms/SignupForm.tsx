6e2306d37047a505930057a14405278a6c11c8d8
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-11-21 20:32:16 +0300

    database progress

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -13,10 +13,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
+import { Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
 
-  const isLoading = true;
+  const isLoading = false;
 
   //1. define your form
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -29,10 +32,10 @@ const SignupForm = () => {
     },
   });
   //2. define a submit handler
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+
+     const newUser = await createUserAccount(values);
+    console.log(newUser)
   }
   return (
     <Form {...form}>
@@ -42,7 +45,7 @@ const SignupForm = () => {
           Create a new account
         </h2>
         <p className=" text-light-3 small-medium md:base-regular mt-12">
-          To use Snapgram enter your account details
+          To use Snapgram, please enter your account details
         </p>
 
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
@@ -101,10 +104,14 @@ const SignupForm = () => {
           <Button className="shad-button_primary" type="submit">
             {isLoading ? (
               <div className="flex-center gap-2">
-                Loading ...
+                <Loader/> Loading...
               </div>
             ): "Sign Up"}
           </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+              Already have an account?
+              <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Login</Link>
+          </p>
         </form>
       </div>
     </Form>
--- src/components/ui/shared/Loader.tsx
+++ src/components/ui/shared/Loader.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex-center w-full">
+      <img src="/assets/icons/loader.svg" alt="loader" width={24} height={24} />
+    </div>
   )
 }
 
